a017238f767b74c2c7ef0f3bd0b98fc5bc24ba61
Karlo Sall <karlo.sall.hansen@gmail.com>
2016-05-16 12:27:15 +0200

    added: frontend handling of all company properties

--- public/app/company.ts
+++ public/app/company.ts
@@ -1,4 +1,9 @@
 export class Company {
     id      : number;
+    cvr     : number;
     name    : string;
+    adress  : string;
+    city    : string;
+    country : string;
+    phone   : number;
 }
